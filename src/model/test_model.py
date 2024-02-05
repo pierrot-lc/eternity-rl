34217d9a6047c616a0d23bea9b46f7fcdc40d8d5
Pierrot LC <pierrotlc@proton.me>
2024-02-05 22:54:57 +0000

    🧪 model: testing for rotation equivariance

--- src/model/test_model.py
+++ src/model/test_model.py
@@ -1,8 +1,127 @@
+import einops
 import pytest
 import torch
 
-from ..environment import N_SIDES
+from ..environment import N_SIDES, EternityEnv, random_perfect_instances
+from .backbone import Backbone
 from .class_encoding import ClassEncoding
+
+
+def init_env(seed: int) -> EternityEnv:
+    generator = torch.Generator().manual_seed(seed)
+    instances = random_perfect_instances(
+        size=4, n_classes=6, n_instances=10, generator=generator
+    )
+    return EternityEnv(instances, episode_length=float("+inf"), device="cpu")
+
+
+def rotate_tensor(tensor: torch.Tensor) -> torch.Tensor:
+    """Rotate the given tensor by 90 degres, assumes the tensor is 2D.
+
+    ---
+    Args:
+        tensor: The 2D tensor to rotate.
+            Shape of [batch_size, y_size, x_size, ...].
+
+    ---
+    Return:
+        The rotated tensor.
+            Shape of [batch_size, x_size, y_size, ...].
+    """
+    tensor = einops.rearrange(tensor, "b y x ... -> b x y ...")
+    tensor = tensor.flip(dims=(1,))
+    return tensor
+
+
+@pytest.mark.parametrize(
+    "tensor, rotated",
+    [
+        (
+            torch.LongTensor(
+                [
+                    [
+                        [1, 2, 3],
+                        [4, 5, 6],
+                    ],
+                    [
+                        [6, 5, 4],
+                        [3, 2, 1],
+                    ],
+                ]
+            ),
+            torch.LongTensor(
+                [
+                    [
+                        [3, 6],
+                        [2, 5],
+                        [1, 4],
+                    ],
+                    [
+                        [4, 1],
+                        [5, 2],
+                        [6, 3],
+                    ],
+                ]
+            ),
+        ),
+        (
+            torch.LongTensor(
+                [
+                    [
+                        [[1, 2], [3, 4]],
+                        [[5, 6], [7, 8]],
+                    ],
+                ]
+            ),
+            torch.LongTensor(
+                [
+                    [
+                        [[3, 4], [7, 8]],
+                        [[1, 2], [5, 6]],
+                    ],
+                ]
+            ),
+        ),
+    ],
+)
+def test_rotate_tensor(tensor: torch.Tensor, rotated: torch.Tensor):
+    tensor = rotate_tensor(tensor)
+    assert torch.all(tensor == rotated)
+
+
+@torch.inference_mode()
+@pytest.mark.parametrize(
+    "boards, backbone",
+    [
+        (
+            init_env(seed=0).reset()[0],
+            Backbone(embedding_dim=12, n_heads=1, n_layers=1, dropout=0.0),
+        ),
+        (
+            init_env(seed=1).reset()[0],
+            Backbone(embedding_dim=12, n_heads=2, n_layers=1, dropout=0.0),
+        ),
+        (
+            init_env(seed=2).reset()[0],
+            Backbone(embedding_dim=12, n_heads=2, n_layers=3, dropout=0.0),
+        ),
+    ],
+)
+def test_equivariant_backbone(boards: torch.Tensor, backbone: Backbone):
+    _, _, height, width = boards.shape
+    encoding = backbone(boards)
+    encoding = einops.rearrange(encoding, "(h w) b e -> b h w e", h=height, w=width)
+
+    for _ in range(4):
+        boards = einops.rearrange(boards, "b s h w -> b h w s")
+        boards = rotate_tensor(boards)
+        boards = einops.rearrange(boards, "b h w s -> b s h w")
+        encoding = rotate_tensor(encoding)
+        rotate_encoding = backbone(boards)
+        rotate_encoding = einops.rearrange(
+            rotate_encoding, "(h w) b e -> b h w e", h=height, w=width
+        )
+        assert torch.allclose(encoding, rotate_encoding, atol=1e-5)
 
 
 @pytest.mark.parametrize(
