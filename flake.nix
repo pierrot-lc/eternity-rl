be452916b1b718ef195cee7ef2ae27aafe479b2b
Pierrot LC <pierrotlc@proton.me>
2024-02-22 12:24:50 +0100

    🎨 format: flake.nix

--- flake.nix
+++ flake.nix
@@ -14,8 +14,10 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-  let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
@@ -25,20 +27,21 @@
       };
     };
 
-    python-packages = ps: with ps; [
-      pip
-      setuptools
-      virtualenv
-    ];
+    python-packages = ps:
+      with ps; [
+        pip
+        setuptools
+        virtualenv
+      ];
 
     fhs = pkgs.buildFHSUserEnv {
       name = "tsp-env";
       targetPkgs = pkgs: (with pkgs; [
-          (python311.withPackages python-packages)
-          cudaPackages.cudatoolkit
-          cudaPackages.cudnn
-          just
-          pdm
+        (python311.withPackages python-packages)
+        cudaPackages.cudatoolkit
+        cudaPackages.cudnn
+        just
+        pdm
       ]);
     };
   in {
