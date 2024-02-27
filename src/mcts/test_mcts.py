🐛 fix: MCTS backprop

src/mcts/test_mcts.py
@@ -477,13 +477,13 @@ def test_repeat_interleave():
             torch.LongTensor([0, 1]),
             torch.LongTensor(
                 [
-                    [0, 2, 1, 1, 1, 1, 0],
+                    [4, 2, 1, 1, 1, 1, 0],
                     [2, 1, 1, 0, 0, 0, 0],
                 ]
             ),
             torch.FloatTensor(
                 [
-                    [0.0000, 2.0000, 0.6000, 0.4000, 1.0000, 1.0000, 0.0000],
+                    [4.3000, 2.0000, 0.6000, 0.4000, 1.0000, 1.0000, 0.0000],
                     [3.1000, 1.1000, 0.9000, 0.0000, 0.0000, 0.0000, 0.0000],
                 ]
             ),
@@ -499,13 +499,13 @@ def test_repeat_interleave():
             torch.LongTensor(
                 [
                     [4, 2, 1, 1, 1, 1, 0],
-                    [0, 1, 1, 0, 0, 0, 0],
+                    [2, 1, 1, 0, 0, 0, 0],
                 ]
             ),
             torch.FloatTensor(
                 [
                     [4.1200, 1.8000, 0.6000, 0.4000, 1.0000, 1.0000, 0.0000],
-                    [0.0000, 1.1000, 0.9000, 0.0000, 0.0000, 0.0000, 0.0000],
+                    [3.1000, 1.1000, 0.9000, 0.0000, 0.0000, 0.0000, 0.0000],
                 ]
             ),
             torch.BoolTensor(
@@ -525,7 +525,6 @@ def test_backpropagate(
 ):
     tree = tree_mockup()
     tree.backpropagate(nodes)
-    print(tree.sum_scores)
 
     assert torch.all(tree.visits == updated_visits), "Wrong visits number"
     assert torch.allclose(
@@ -540,7 +539,7 @@ def test_all_terminated():
 
     tree = tree_mockup_small()
     tree.envs, tree.policy, tree.critic = env, policy, critic
-    tree.terminated[0] = True
+    tree.terminated[0, ...] = True
 
     actions_ = tree.actions.clone()
     childs_ = tree.childs.clone()
@@ -567,8 +566,6 @@ def test_all_terminated():
     assert torch.all(terminated_[to_ignore] == tree.terminated[to_ignore])
     assert torch.all(values_[to_ignore] == tree.values[to_ignore])
     assert torch.all(visits_[to_ignore] == tree.visits[to_ignore])
-
-    # TODO: Make sure the backprop is done correctly.
 
     # Make sure the step does not break.
     tree.step()

src/mcts/tree.py
@@ -457,10 +457,11 @@ class MCTSTree:
         actions = self.actions.scatter(dim=1, index=childs_node_id, src=actions)
         self.actions[to_modify] = actions[to_modify]
 
-    def update_nodes_info(self, nodes: torch.Tensor, filters: torch.Tensor):
+    def update_nodes_info(self, nodes: torch.Tensor):
         """Update the information of the given nodes.
 
-        If a node is masked (filter is False), its value is not updated.
+        Note that this method can be called multiple times on the
+        same nodes as it won't change their values after the first call.
         """
         childs = self.childs[self.batch_range, nodes]
 
@@ -473,7 +474,7 @@ class MCTSTree:
         # Set the visits as the sum of the children visits.
         visits = torch.gather(self.visits, dim=1, index=childs)
         visits[childs == 0] = 0
-        self.visits[self.batch_range, nodes] = visits.sum(dim=1) * filters
+        self.visits[self.batch_range, nodes] = visits.sum(dim=1)
 
         # Same for `sum_scores`.
         sum_scores = torch.gather(self.sum_scores, dim=1, index=childs)
@@ -481,7 +482,7 @@ class MCTSTree:
         sum_scores[childs == 0] = 0.0
         rewards[childs == 0] = 0.0
         scores = rewards + self.gamma * sum_scores
-        self.sum_scores[self.batch_range, nodes] = scores.sum(dim=1) * filters
+        self.sum_scores[self.batch_range, nodes] = scores.sum(dim=1)
 
     def backpropagate(self, leafs: torch.Tensor):
         """Backpropagate the new value estimate from the given leafs to their parents.
@@ -493,16 +494,12 @@ class MCTSTree:
         # We maintain a list of node to exclude so that we do not update
         # twice a root node.
         nodes = leafs
-        filters = nodes != 0
 
         # While we did not update all root nodes.
-        while not torch.all(~filters):
+        while not torch.all(nodes == 0):
             # NOTE: root nodes are their own parents.
             nodes = self.parents[self.batch_range, nodes]
-            self.update_nodes_info(nodes, filters)
-
-            # Do not update twice a root node.
-            filters = nodes != 0
+            self.update_nodes_info(nodes)
 
     @property
     def tree_nodes(self) -> torch.Tensor: